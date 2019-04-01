a9591ccbd6d6c79908c32423555667f8379e3591
MarioAlbertoNL1799 <1717110635@utectulancingo.edu.mx>
2019-04-01 17:20:27 -0600

    Se crean más interfaces acorde al proyecto Se modifica diseño de aplicacion(colores) Se inicia la relacion entre actividades.

---
+++ appMovil/app/src/main/java/com/example/fanny/advihawk/Perfil_Asesor.java
@@ -0,0 +1,15 @@
+package com.example.fanny.advihawk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Perfil_Asesor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
